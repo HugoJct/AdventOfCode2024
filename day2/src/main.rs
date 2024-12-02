6193bab08d79ffa9bfa31fc5e8a6a2433e4230d8
Hugo Jacotot <hjacotot@protonmail.com>
2024-12-02 14:31:08 +0100

    Day 2 Part. 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -9,7 +9,21 @@ fn main() {
                 .map(|x| x.parse::<i32>().unwrap())
                 .collect::<Vec<i32>>()
         })
-        .map(|x| only_increasing(&x) || only_decreasing(&x))
+        .map(|x| {
+            if only_increasing(&x) || only_decreasing(&x) {
+                return true;
+            }
+
+            for (i, _) in x.clone().into_iter().enumerate() {       // test the array again by
+                // removing all the element one by one to find a working combination
+                let mut tmp = x.clone();
+                tmp.remove(i);
+                if only_increasing(&tmp) || only_decreasing(&tmp) {
+                    return true;
+                }
+            }
+            false
+        })
         .filter(|x| x == &true)
         .count();
 
@@ -20,7 +34,7 @@ fn only_increasing(line: &Vec<i32>) -> bool {
     !line
         .clone()
         .into_iter()
-        .zip(line.into_iter().skip(1))
+        .zip(line.clone().into_iter().skip(1))
         .map(|(one, two)| {
             let diff = one - two;
             diff >= 1 && diff <= 3
