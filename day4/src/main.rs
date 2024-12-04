cd91fdf0029cedb48e9345ca210490b7246b3f4d
Hugo Jacotot <hjacotot@protonmail.com>
2024-12-04 23:05:28 +0100

    Day 4 Part. 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-use diagonal::{diagonal_pos_neg, diagonal_pos_pos, straight_y};
+use diagonal::straight_y;
 use regex::Regex;
 
 fn main() {
@@ -17,44 +17,54 @@ fn main() {
             })
     });
 
-    let input_diag = diagonal_pos_pos(&input).into_iter().map(|l| {
-        l.into_iter()
-            .filter_map(|x| char::from_u32(*x as u32))
-            .fold(String::from(""), |mut acc, x| {
-                acc.push(x);
-                acc
-            })
-    });
+    let regex_first_third_line = Regex::new(r"^M.S").unwrap();
+    let regex_second_line = Regex::new(r"^.A.").unwrap();
 
-    let input_diag_rev = diagonal_pos_neg(&input).into_iter().map(|l| {
-        l.into_iter()
-            .filter_map(|x| char::from_u32(*x as u32))
-            .fold(String::from(""), |mut acc, x| {
-                acc.push(x);
-                acc
-            })
-    });
-
-    let regex = Regex::new(r"XMAS").unwrap();
-    let regex_rev = Regex::new(r"SAMX").unwrap();
+    let regex_first_third_line_rev = Regex::new(r"^S.M").unwrap();
 
     let mut total: u32 = 0;
 
-    total += input.clone().into_iter().fold(0, |acc, l| {
-        acc + regex.find_iter(l).count() as u32 + regex_rev.find_iter(l).count() as u32
-    });
+    input
+        .clone()
+        .into_iter()
+        .zip(input.clone().into_iter().skip(1))
+        .zip(input.clone().into_iter().skip(2))
+        .for_each(|((one, two), three)| {
+            for i in 0..one.len() {
+                if regex_first_third_line.is_match(&one[i..])
+                    && regex_second_line.is_match(&two[i..])
+                    && regex_first_third_line.is_match(&three[i..])
+                {
+                    total += 1;
+                } else if regex_first_third_line_rev.is_match(&one[i..])
+                    && regex_second_line.is_match(&two[i..])
+                    && regex_first_third_line_rev.is_match(&three[i..])
+                {
+                    total += 1;
+                }
+            }
+        });
 
-    total += input_rev.fold(0, |acc, l| {
-        acc + regex.find_iter(&l).count() as u32 + regex_rev.find_iter(&l).count() as u32
-    });
-
-    total += input_diag.fold(0, |acc, l| {
-        acc + regex.find_iter(&l).count() as u32 + regex_rev.find_iter(&l).count() as u32
-    });
-
-    total += input_diag_rev.fold(0, |acc, l| {
-        acc + regex.find_iter(&l).count() as u32 + regex_rev.find_iter(&l).count() as u32
-    });
+    input_rev
+        .clone()
+        .into_iter()
+        .zip(input_rev.clone().into_iter().skip(1))
+        .zip(input_rev.clone().into_iter().skip(2))
+        .for_each(|((one, two), three)| {
+            for i in 0..one.len() {
+                if regex_first_third_line.is_match(&one[i..])
+                    && regex_second_line.is_match(&two[i..])
+                    && regex_first_third_line.is_match(&three[i..])
+                {
+                    total += 1;
+                } else if regex_first_third_line_rev.is_match(&one[i..])
+                    && regex_second_line.is_match(&two[i..])
+                    && regex_first_third_line_rev.is_match(&three[i..])
+                {
+                    total += 1;
+                }
+            }
+        });
 
     println!("{}", total);
 }
