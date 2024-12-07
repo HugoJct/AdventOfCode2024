d38fac69f20a014187dfe06ac0598f260366b2cd
Hugo Jacotot <hjacotot@protonmail.com>
2024-12-07 16:12:58 +0100

    Day 7 Part. 1

---
+++ day7/src/main.rs
@@ -0,0 +1,53 @@
+use std::fs;
+
+fn main() {
+    let combinations: Vec<(u64, Vec<u64>)> = fs::read_to_string("res/input")
+        .unwrap()
+        .lines()
+        .map(|l| {
+            let (result_str, operands_str) = l.split_once(":").unwrap();
+            let result: u64 = result_str.parse().unwrap();
+            let operands: Vec<u64> = operands_str
+                .split(" ")
+                .filter_map(|op| match op.parse::<u64>() { Ok(x) => Some(x), Err(_) => None,
+                })
+                .collect();
+
+            (result, operands)
+        })
+        .collect();
+
+    let sum: u64 = combinations
+        .iter()
+        .filter_map(|(total, operands)| {
+            if verify(*total, &operands) {
+                return Some(total);
+            }
+            None
+        })
+        .sum();
+
+    println!("{sum}");
+}
+
+fn verify(total: u64, operands: &[u64]) -> bool {
+    if operands.len() == 1 {
+        return total == operands[0];
+    }
+    // println!("{:?}", operands);
+
+    let mut mul: bool = false;
+    let mut add: bool = false;
+
+    let operand = operands[operands.len() - 1];
+
+    if total % operand == 0 {
+        mul =  verify(total / operand, &operands[..(operands.len() - 1)]);
+    } 
+
+    if total >= operand {
+        add =  verify(total - operand, &operands[..(operands.len() - 1)]);
+    } 
+
+    return mul || add;
+}
