719824c9319edf5b59a832582a9d23e5caa988f6
Hugo Jacotot <hjacotot@protonmail.com>
2024-12-10 16:16:05 +0100

    Day 10 Part. 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,33 +1,34 @@
 use std::fs;
 
-fn ways_to_top(grid: &Vec<Vec<u32>>, start: (usize, usize), acc: &mut Vec<(usize, usize)>) {
+fn ways_to_top(grid: &Vec<Vec<u32>>, start: (usize, usize)) -> u32 {
     let (x, y) = start;
 
-    if grid[x][y] == 9 && acc.contains(&(x, y)) == false {
-        acc.push((x, y));
+    if grid[x][y] == 9 {
+        return 1;
     }
 
+    let mut left_count: u32 = 0;
+    let mut right_count: u32 = 0;
+    let mut bot_count: u32 = 0;
+    let mut top_count: u32 = 0;
+
     if x > 0 && grid[x - 1][y] == grid[x][y] + 1 {
-        ways_to_top(grid, ((x - 1), y), acc)
+        left_count = ways_to_top(grid, ((x - 1), y))
     }
 
     if x < grid.len() - 1 && grid[x + 1][y] == grid[x][y] + 1 {
-        ways_to_top(grid, ((x + 1), y), acc)
+        right_count = ways_to_top(grid, ((x + 1), y))
     }
 
     if y > 0 && grid[x][y - 1] == grid[x][y] + 1 {
-        ways_to_top(grid, (x, (y - 1)), acc)
+        top_count = ways_to_top(grid, (x, (y - 1)))
     }
 
     if y < grid.len() - 1 && grid[x][y + 1] == grid[x][y] + 1 {
-        ways_to_top(grid, (x, (y + 1)), acc);
+        bot_count = ways_to_top(grid, (x, (y + 1)))
     }
-}
 
-fn tops_reachable(grid: &Vec<Vec<u32>>, start: (usize, usize)) -> u32 {
-    let mut acc: Vec<(usize, usize)> = vec![];
-    ways_to_top(grid, start, &mut acc);
-    acc.len() as u32
+    left_count + right_count + bot_count + top_count
 }
 
 fn main() {
@@ -44,10 +45,11 @@ fn main() {
     for i in 0..input.len() {
         for j in 0..input[i].len() {
             if input[i][j] == 0 {
-                let a = tops_reachable(&input, (i, j));
+                let a = ways_to_top(&input, (i, j));
                 total += a;
             }
         }
     }
+
     println!("{}", total);
 }
