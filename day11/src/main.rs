3194fb9fd7585b2f4c282461eceaba54b73828bd
Hugo Jacotot <hjacotot@protonmail.com>
2024-12-11 18:22:14 +0100

    Day 11 Part. 2

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,42 +1,35 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 fn main() {
     println!("Hello, world!");
 
-    let mut input: Vec<Vec<u64>> = fs::read_to_string("res/input")
+    let mut times: HashMap<u64, u64> = HashMap::new();
+
+    fs::read_to_string("res/input")
         .unwrap()
         .lines()
-        .map(|l| {
-            l.split(" ")
-                .into_iter()
-                .map(|c| c.parse().unwrap())
-                .collect()
-        })
-        .collect();
+        .for_each(|l| {
+            l.split(" ").into_iter().for_each(|c| {
+                *times.entry(c.parse().unwrap()).or_insert(0) += 1;
+            });
+        });
 
-    for _ in 0..25 {
-        input[0] = input[0]
-            .iter()
-            .flat_map(|i| {
-                let mut vec: Vec<u64> = vec![];
-                if *i == 0 {
-                    vec.push(1)
-                } else if i.to_string().len() % 2 == 0 {
-                    let istr = i.to_string();
-                    vec.push(istr[..istr.len() / 2].parse().unwrap());
-                    vec.push(istr[(istr.len() / 2)..].parse().unwrap());
-                } else {
-                    vec.push(*i * 2024)
-                }
-                vec
-            })
-            .collect();
+    for _ in 0..75 {
+        let mut new_map: HashMap<u64, u64> = HashMap::new();
 
-    //     for e in &input[0] {
-    //         print!("{} ", e);
-    //     }
-    //
-    // println!("");
+        for (&k, &v) in &times {
+            if k == 0 {
+                *new_map.entry(1).or_insert(0) += v;
+            } else if k.to_string().len() % 2 == 0 {
+                let istr = k.to_string();
+                *new_map.entry(istr[..istr.len() / 2].parse::<u64>().unwrap()).or_insert(0) += v;
+                *new_map.entry(istr[(istr.len() / 2)..].parse::<u64>().unwrap()).or_insert(0) += v;
+            } else {
+                *new_map.entry(k * 2024).or_insert(0) += v;
+            }
+        }
+        times = new_map;
     }
-    println!("{}", input[0].len());
+
+    println!("{}", times.values().sum::<u64>());
 }
